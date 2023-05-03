c65776b56e61aab0ce3ebea4e89defc9893f5ce3
jnovillo <98795134+jnovillo@users.noreply.github.com>
2023-05-02 22:47:41 -0400

    Update BankAccount.java

--- docs/projects/inheritance/student_1/BankAccount.java
+++ docs/projects/inheritance/student_1/BankAccount.java
@@ -1,23 +1,3 @@
-interface BankAccountInterface {
-  double getBalance();
-
-  String getName();
-
-  void setBalance(double balance);
-
-  void deposit(double amount) throws Exception;
-
-  void withdraw(double amount) throws Exception;
-
-  void addInterest() throws Exception;
-
-  int getWithdrawCount();
-
-  void withdraw(double amount, String parentName) throws Exception;
-
-  void transfer(double amount, BankAccount destination) throws Exception;
-}
-
 abstract class BankAccount implements BankAccountInterface {
   private String name;
   private double balance;
